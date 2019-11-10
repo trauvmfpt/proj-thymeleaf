b2297a4f77818db060502b69214734dc7005d270
hoangnvd00536@fpt.edu.vn <hoangnvd00536@fpt.edu.vn>
2019-11-10 18:07:11 +0700

    fix db name

--- src/main/java/com/fpt/t1708e/photoplatform/PhotoplatformApplication.java
+++ src/main/java/com/fpt/t1708e/photoplatform/PhotoplatformApplication.java
@@ -2,12 +2,17 @@ package com.fpt.t1708e.photoplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhotoplatformApplication {
+public class PhotoplatformApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(PhotoplatformApplication.class, args);
     }
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PhotoplatformApplication.class);
+    }
 }

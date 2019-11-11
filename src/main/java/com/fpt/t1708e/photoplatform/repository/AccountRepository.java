01af8e7a431393df03ce80138c3d1ab331f8a597
Nguyen.Van.Hoang2 <Nguyen.Van.Hoang2@usol-v.com.vn>
2019-11-11 16:07:25 +0700

    seeder for user

--- src/main/java/com/fpt/t1708e/photoplatform/repository/AccountRepository.java
+++ src/main/java/com/fpt/t1708e/photoplatform/repository/AccountRepository.java
@@ -3,8 +3,10 @@ package com.fpt.t1708e.photoplatform.repository;
 import com.fpt.t1708e.photoplatform.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
-    Optional<Account> findStudentByUsername(String username);
+    Optional<List<Account>> findAllAccountByRole(int role);
+    
 }

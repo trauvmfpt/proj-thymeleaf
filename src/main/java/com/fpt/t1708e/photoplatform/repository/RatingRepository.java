a0b1770740f17fa5e6ba875a192852e23d5e2605
JUstSaLtPiLe <vu.tienphuc97@gmail.com>
2019-11-14 11:29:02 +0700

    done

--- src/main/java/com/fpt/t1708e/photoplatform/repository/RatingRepository.java
+++ src/main/java/com/fpt/t1708e/photoplatform/repository/RatingRepository.java
@@ -1,8 +1,20 @@
 package com.fpt.t1708e.photoplatform.repository;
 
 import com.fpt.t1708e.photoplatform.entity.Album;
+import com.fpt.t1708e.photoplatform.entity.Comment;
 import com.fpt.t1708e.photoplatform.entity.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RatingRepository extends JpaRepository<Rating,Long> {
+    @Query("SELECT c FROM Rating r where r.album.id = :id or r.product.id = :id or r.studioInfo.id = :id or r.photographerInfo.id = :id")
+    List<Rating> findByPostId(long id);
+
+    @Query("SELECT c FROM Rating r where (r.customerInfo.id = :accountId and r.product.id = :postId)" +
+            "or (r.customerInfo.id = :accountId and r.album.id = :postId)" +
+            "or (r.customerInfo.id = :accountId and r.photographerInfo.id = :postId) " +
+            "or (r.customerInfo.id = :accountId and r.studioInfo.id = :postId)")
+    Rating findByUserIdAndPostId(long accountId, long postId);
 }

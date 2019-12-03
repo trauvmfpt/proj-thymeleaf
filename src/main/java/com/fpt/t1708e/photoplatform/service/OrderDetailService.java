d9874fdd273d494030e71b33eb045bcc47b17544
anhlhpd00579 <anhlhpd00579@fpt.edu.vn>
2019-12-03 21:15:13 +0700

    Sửa hiển thị cột trang order detail

--- src/main/java/com/fpt/t1708e/photoplatform/service/OrderDetailService.java
+++ src/main/java/com/fpt/t1708e/photoplatform/service/OrderDetailService.java
@@ -1,7 +1,6 @@
 package com.fpt.t1708e.photoplatform.service;
 
 import com.fpt.t1708e.photoplatform.entity.OrderDetail;
-import com.fpt.t1708e.photoplatform.entity.OrderProduct;
 import com.fpt.t1708e.photoplatform.repository.OrderDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;

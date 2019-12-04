9f7d88fae94b242c4763d91cab6c9b47bc70b3f3
datto <dattq1701@gmail.com>
2019-12-04 14:17:09 +0700

    Thêm seeder

---
+++ src/main/java/com/fpt/t1708e/photoplatform/seeder/PhotographerInfoSeeder.java
@@ -0,0 +1,25 @@
+package com.fpt.t1708e.photoplatform.seeder;
+
+import com.fpt.t1708e.photoplatform.entity.PhotographerInfo;
+import com.fpt.t1708e.photoplatform.entity.StudioInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotographerInfoSeeder {
+    public static List<PhotographerInfo> photographerInfos = new ArrayList<>();
+    public void addPtg(){
+        PhotographerInfo photographerInfo = new PhotographerInfo();
+        //
+//        photographerInfo.setAvatar();
+//        photographerInfo.setAddress();
+//        photographerInfo.setDescription();
+//        photographerInfo.setEmail();
+//        photographerInfo.setFullName();
+//        photographerInfo.setPhone();
+//        photographerInfos.add(photographerInfo);
+        //
+
+
+    }
+}
---
+++ src/main/java/com/fpt/t1708e/photoplatform/seeder/ProductSeeder.java
@@ -0,0 +1,90 @@
+package com.fpt.t1708e.photoplatform.seeder;
+
+import com.fpt.t1708e.photoplatform.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSeeder {
+    public static List<Product> productList = new ArrayList<>();
+    public void addProduct(){
+        Product product = new Product();
+        // 1. - Tuarts
+        product.setName("Chụp ảnh cưới");
+        product.setContent("Tour chụp ảnh cưới hot nhất năm 2019 – 2020");
+        product.setDescription("
+            Các cặp đôi đăng kí hợp đồng đặt lịch chụp trong khoảng từ 25.12 – 31.12.2019 sẽ nhận đồng thời các ƯU ĐÃI ĐẶC BIỆT:
+                ✔ TẶNG ngay 1.000.000 VND
+                ✔ MƯỢN MIỄN PHÍ váy cưới trị giá 4.000.000 thương hiệu váy cưới Bella Bridal.
+                ✔ TẶNG tiếp#Voucher trị giá 3.000.000 VND khi mua nhẫn cưới tại thương hiệu trang sức cao cấp Cửu Long Jewelry.
+                ✔ TẶNG tiếp #Voucher giảm 20% khi mua Vest cưới đến từ Thương hiệu Adam Store.
+                ✔ TẶNG tiếp #Voucher giảm 15% khi mua nhẫn cưới đến từ Thương hiệu Huy Thanh Jewelry.
+                ✔ TẶNG tiếp 500,000 VND khi đặt cổng hoa, thuê bàn ghế, hoa để bàn trị giá 1.000.000 VND đến từ thương hiệu Viet Anh Design Wedding Planer.
+        ");
+        product.setDestination("");
+        product.setArea("Vũng Tàu");
+        product.setPrice("25000000");
+        product.setPriceDiscount("22000000");
+        product.setThumbnail("");
+
+        // 2. - Tuarts
+        product.setName("Chụp ảnh cưới");
+        product.setContent("Tour chụp ảnh cưới hot nhất năm 2019 – 2020");
+        product.setDescription("
+            Từ 13.01.2020 – 20.01.2020. TuArt Team sẽ có mặt tại Mộc Châu với các gói chụp ưu đãi như sau:
+                ️✔ KHUYẾN MẠI Tour chụp ảnh cưới tại Mộc Châu. Chi phí còn lại: 15.500.000 VND/ ngày
+                ✔ Chi phí này Đã bao gồm chi phí đi lại từ Hà Nội – Mộc Châu, ăn ở của Ekip chụp, Quý khách hàng không phải lo chi phí này cho Ekip chụp hình nữa.
+        ");
+        product.setDestination("");
+        product.setArea("Mộc Châu");
+        product.setPrice("15500000");
+        product.setPriceDiscount("15500000");
+        product.setThumbnail("");
+
+         // 3. - Tuarts
+        product.setName("Chụp ảnh cưới");
+        product.setContent("Tour chụp ảnh cưới hot nhất năm 2019 – 2020");
+        product.setDescription("
+            Các cặp đôi đăng kí trong khoảng từ 24.12 – 31.12.2019 sẽ nhận đồng thời các ƯU ĐÃI ĐẶC BIỆT:
+                ✔ TẶNG ngay 1.000.000 VND
+                ✔ MƯỢN MIỄN PHÍ váy cưới trị giá 4.000.000 thương hiệu váy cưới Bella Bridal.
+                ✔ TẶNG tiếp#Voucher trị giá 3.000.000 VND khi mua nhẫn cưới tại thương hiệu trang sức cao cấp Cửu Long Jewelry.
+                ✔ TẶNG tiếp #Voucher giảm 20% khi mua Vest cưới đến từ Thương hiệu Adam Store.
+                ✔ TẶNG tiếp #Voucher giảm 20% khi mua nhẫn cưới đến từ Thương hiệu Huy Thanh Jewelry.
+                ✔ TẶNG tiếp 500,000 VND khi đặt cổng hoa, thuê bàn ghế, hoa để bàn trị giá 1.000.000 VND đến từ thương hiệu Viet Anh Design Wedding Planer.
+        ");
+        product.setDestination("");
+        product.setArea("Tam Đảo");
+        product.setPrice("17000000");
+        product.setPriceDiscount("1500000");
+        product.setThumbnail("");
+
+         // 4. - Tuarts
+        product.setName("Thuê váy cưới");
+        product.setContent("Xu hướng váy cưới đẹp mùa cưới 2019 - 2020");
+        product.setDescription("
+            Từ 13.01.2020 – 20.01.2020. TuArt Team sẽ có mặt tại Mộc Châu với các gói chụp ưu đãi như sau:
+            ️✔ KHUYẾN MẠI Tour chụp ảnh cưới tại Mộc Châu. Chi phí còn lại: 15.500.000 VND/ ngày
+            ✔ Chi phí này Đã bao gồm chi phí đi lại từ Hà Nội – Mộc Châu, ăn ở của Ekip chụp, Quý khách hàng không phải lo chi phí này cho Ekip chụp hình nữa.
+        ");
+        product.setDestination("");
+        product.setArea("Tuart");
+        product.setPrice("15500000");
+        product.setPriceDiscount("15500000");
+        product.setThumbnail("");
+
+         // 5. - Tuarts
+        product.setName("Chụp ảnh cưới");
+        product.setContent("Tour chụp ảnh cưới hot nhất năm 2019 – 2020");
+        product.setDescription("
+            Từ 13.01.2020 – 20.01.2020. TuArt Team sẽ có mặt tại Mộc Châu với các gói chụp ưu đãi như sau:
+            ️✔ KHUYẾN MẠI Tour chụp ảnh cưới tại Mộc Châu. Chi phí còn lại: 15.500.000 VND/ ngày
+            ✔ Chi phí này Đã bao gồm chi phí đi lại từ Hà Nội – Mộc Châu, ăn ở của Ekip chụp, Quý khách hàng không phải lo chi phí này cho Ekip chụp hình nữa.
+        ");
+        product.setDestination("");
+        product.setArea("Tam Đảo");
+        product.setPrice("15500000");
+        product.setPriceDiscount("15500000");
+        product.setThumbnail("");
+    }
+}

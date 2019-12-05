d2541b9a74cbf0ddb03731b6cff1d08636c7675d
Nguyen.Van.Hoang2 <Nguyen.Van.Hoang2@usol-v.com.vn>
2019-12-05 14:23:12 +0700

    add order in seed

--- src/main/java/com/fpt/t1708e/photoplatform/seeder/PhotographerInfoSeeder.java
+++ src/main/java/com/fpt/t1708e/photoplatform/seeder/PhotographerInfoSeeder.java
@@ -8,62 +8,57 @@ import java.util.List;
 
 public class PhotographerInfoSeeder {
     public static List<PhotographerInfo> photographerInfos = new ArrayList<>();
-    public void addPtg(){
-//        PhotographerInfo photographerInfo = new PhotographerInfo();
-//        https://www.elleman.vn/hot-women/nu-nhiep-anh-gia-viet-nam-tre?fbclid=IwAR2t4fUht4Zd2UJktatHFNF-tV58Lofs2GG5k2wgMbr8QG7Ld8a7qe67FjQ
+    public static void addPtg(){
+        PhotographerInfo photographerInfo = new PhotographerInfo();
+        https://www.elleman.vn/hot-women/nu-nhiep-anh-gia-viet-nam-tre?fbclid=IwAR2t4fUht4Zd2UJktatHFNF-tV58Lofs2GG5k2wgMbr8QG7Ld8a7qe67FjQ
 
-       // 1.
-    //    photographerInfo.setFullName("Dạ Miêu");
-    //    photographerInfo.setEmail("damieuphotography@gmail.com");
-    //    photographerInfo.setPhone("");
-    //    photographerInfo.setAddress("https://www.instagram.com/da.mieu/");
-    //    photographerInfo.setDescription("
-    //        Dạ Miêu (tên thật là Nguyễn Diệu Linh – 1995) tập tành nhiếp ảnh và làm quen với Photoshop khi chỉ mới là học sinh cấp 2.
-    //        Tình yêu với nghệ thuật cầm máy của tôi chưa phút giây nào chập chờn vụt tắt và nó còn lớn hơn nữa theo chiều dài thời gian.
-    //        Những bức ảnh của tôi luôn mang vẻ đẹp quyến rũ, sang trọng và có chút ma mị nhưng vẫn thấp thoáng đâu đó nét dịu dàng, đằm thắm của người con gái Việt Nam.
-    //        Là một nữ nhiếp ảnh gia, tôi mang niềm nhiệt huyết tuổi trẻ của mình để vun đắp hạnh phúc cho cộng đồng qua những bức ảnh.
-    //    ");
-    //    photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471055/glsy2scgoir3es88lvyw.jpg");
-    //    photographerInfos.add(photographerInfo);
+//        1.
+        photographerInfo.setFullName("Dạ Miêu");
+        photographerInfo.setEmail("damieuphotography@gmail.com");
+        photographerInfo.setPhone("");
+        photographerInfo.setAddress("https://www.instagram.com/da.mieu/");
+        photographerInfo.setDescription("Dạ Miêu (tên thật là Nguyễn Diệu Linh – 1995) tập tành nhiếp ảnh và làm quen với Photoshop khi chỉ mới là học sinh cấp 2.\n" +
+                "            Tình yêu với nghệ thuật cầm máy của tôi chưa phút giây nào chập chờn vụt tắt và nó còn lớn hơn nữa theo chiều dài thời gian.\n" +
+                "            Những bức ảnh của tôi luôn mang vẻ đẹp quyến rũ, sang trọng và có chút ma mị nhưng vẫn thấp thoáng đâu đó nét dịu dàng, đằm thắm của người con gái Việt Nam.\n" +
+                "            Là một nữ nhiếp ảnh gia, tôi mang niềm nhiệt huyết tuổi trẻ của mình để vun đắp hạnh phúc cho cộng đồng qua những bức ảnh.");
+        photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471055/glsy2scgoir3es88lvyw.jpg");
+        photographerInfos.add(photographerInfo);
 
-    //    // 2.
-    //    photographerInfo.setFullName("Trần Hoài An");
-    //    photographerInfo.setEmail("tranhoaian.kinhotrong@gmail.com");
-    //    photographerInfo.setPhone("+84 0347724245");
-    //    photographerInfo.setAddress("https://www.instagram.com/kinhotrong/");
-    //    photographerInfo.setDescription("
-    //    Xúc cảm là bản âm hưởng vu vương và là linh hồn của những bức ảnh của nữ nhiếp ảnh gia tài năng Trần Hoài An (1992).
-    //    Bạn dễ dàng bắt gặp hình ảnh nụ cười hạnh phúc, ánh mắt biết nói, những cái hôn nồng thắm hoặc chỉ đơn giản là một cái chạm nhẹ cũng vô cùng chân thật và sống động.
-    //    Mặc dù những bức ảnh của cô mang màu sắc khá đa dạng, từ những cặp đôi ngọt ngào, ảnh nude gợi cảm cho đến những thiếu nữ đẹp dịu dàng nhưng chất chứa nhiều nỗi niềm trong tâm hồn, nhưng chung quy lại, cảm xúc mới chính thứ được bắt trọn trong những bộ ảnh của cô.
-    //    ");
-    //    photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471055/a08dlj9wxavdhzz8cijs.jpg");
-    //    photographerInfos.add(photographerInfo);
+        // 2.
+        photographerInfo = new PhotographerInfo();
+        photographerInfo.setFullName("Trần Hoài An");
+        photographerInfo.setEmail("tranhoaian.kinhotrong@gmail.com");
+        photographerInfo.setPhone("+84 0347724245");
+        photographerInfo.setAddress("https://www.instagram.com/kinhotrong/");
+        photographerInfo.setDescription("Xúc cảm là bản âm hưởng vu vương và là linh hồn của những bức ảnh của nữ nhiếp ảnh gia tài năng Trần Hoài An (1992).\n" +
+                "        Bạn dễ dàng bắt gặp hình ảnh nụ cười hạnh phúc, ánh mắt biết nói, những cái hôn nồng thắm hoặc chỉ đơn giản là một cái chạm nhẹ cũng vô cùng chân thật và sống động.\n" +
+                "        Mặc dù những bức ảnh của cô mang màu sắc khá đa dạng, từ những cặp đôi ngọt ngào, ảnh nude gợi cảm cho đến những thiếu nữ đẹp dịu dàng nhưng chất chứa nhiều nỗi niềm trong tâm hồn, nhưng chung quy lại, cảm xúc mới chính thứ được bắt trọn trong những bộ ảnh của cô.");
+        photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471055/a08dlj9wxavdhzz8cijs.jpg");
+        photographerInfos.add(photographerInfo);
 
-    //    // 3.
-    //    photographerInfo.setFullName("Dy Duyên");
-    //    photographerInfo.setEmail("dyduyen@gmail.com");
-    //    photographerInfo.setPhone("");
-    //    photographerInfo.setAddress("https://www.instagram.com/dyduyen/");
-    //    photographerInfo.setDescription("
-    //    Màu sắc tươi sáng, bắt mắt, tự nhiên và tối giản là những đặc điểm nổi bật trong những thước ảnh nghệ thuật đầy sáng tạo của nữ nhiếp ảnh gia Dy Duyên (1987).
-    //    Dùng hình ảnh để tâm sự, để nói lên những cảm xúc trực trào là điều Dy Duyên đã và đang theo đuổi.
-    //    Con đường nào dẫn đến thành công cũng chẳng bao giờ trải đầy hoa hồng, một chút thách thức của cuộc đời giúp Dy Duyên tự hào hơn về công việc của mình và trân quý những ngày tháng đã qua để bước xa hơn nữa.
-    //    ");
-    //    photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471055/riicha2vt8qenjbg3g5f.jpg");
-    //    photographerInfos.add(photographerInfo);
+        // 3.
+        photographerInfo = new PhotographerInfo();
+        photographerInfo.setFullName("Dy Duyên");
+        photographerInfo.setEmail("dyduyen@gmail.com");
+        photographerInfo.setPhone("");
+        photographerInfo.setAddress("https://www.instagram.com/dyduyen/");
+        photographerInfo.setDescription("Màu sắc tươi sáng, bắt mắt, tự nhiên và tối giản là những đặc điểm nổi bật trong những thước ảnh nghệ thuật đầy sáng tạo của nữ nhiếp ảnh gia Dy Duyên (1987).\n" +
+                "        Dùng hình ảnh để tâm sự, để nói lên những cảm xúc trực trào là điều Dy Duyên đã và đang theo đuổi.\n" +
+                "        Con đường nào dẫn đến thành công cũng chẳng bao giờ trải đầy hoa hồng, một chút thách thức của cuộc đời giúp Dy Duyên tự hào hơn về công việc của mình và trân quý những ngày tháng đã qua để bước xa hơn nữa.");
+        photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471055/riicha2vt8qenjbg3g5f.jpg");
+        photographerInfos.add(photographerInfo);
 
-    //    // 4.
-    //    photographerInfo.setFullName("Trịnh Mai Phương");
-    //    photographerInfo.setEmail("planniestudio@gmail.com");
-    //    photographerInfo.setPhone("088 998 3288");
-    //    photographerInfo.setAddress("https://www.instagram.com/mphgtrnh/");
-    //    photographerInfo.setDescription("
-    //        Nhiều lần cộng tác với các báo và tạp chí uy tín, nữ nhiếp ảnh gia Trịnh Mai Phương (1991) là cái tên khá nổi trong hội cuồng nhiếp ảnh Việt Nam.
-    //        Cô gái Hà thành này có ngoại hình không thể lẫn vào đâu được với bộ tóc pixie, đôi mắt hí dễ thương cùng phong cách đời thường cá tính và có chút điệu đà nữ tính.
-    //        Những hình ảnh của cô luôn mang vẻ đẹp hiện đại, quý phái và nhã nhặn của phái đẹp Việt Nam.
-    //        Bên cạnh đó, sự hòa quyện hoàn hảo giữa các gam màu cùng đường nét uyển chuyển tạo nên một tổng thể ấn tượng khiến người xem khó lòng rời mắt.
-    //    ");
-    //    photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471054/ibogj5niy4vxnqv2b71m.jpg");
-    //    photographerInfos.add(photographerInfo);
+        // 4.
+        photographerInfo = new PhotographerInfo();
+        photographerInfo.setFullName("Trịnh Mai Phương");
+        photographerInfo.setEmail("planniestudio@gmail.com");
+        photographerInfo.setPhone("088 998 3288");
+        photographerInfo.setAddress("https://www.instagram.com/mphgtrnh/");
+        photographerInfo.setDescription("Nhiều lần cộng tác với các báo và tạp chí uy tín, nữ nhiếp ảnh gia Trịnh Mai Phương (1991) là cái tên khá nổi trong hội cuồng nhiếp ảnh Việt Nam.\n" +
+                "            Cô gái Hà thành này có ngoại hình không thể lẫn vào đâu được với bộ tóc pixie, đôi mắt hí dễ thương cùng phong cách đời thường cá tính và có chút điệu đà nữ tính.\n" +
+                "            Những hình ảnh của cô luôn mang vẻ đẹp hiện đại, quý phái và nhã nhặn của phái đẹp Việt Nam.\n" +
+                "            Bên cạnh đó, sự hòa quyện hoàn hảo giữa các gam màu cùng đường nét uyển chuyển tạo nên một tổng thể ấn tượng khiến người xem khó lòng rời mắt.");
+        photographerInfo.setAvatar("https://res.cloudinary.com/phucvu/image/upload/v1575471054/ibogj5niy4vxnqv2b71m.jpg");
+        photographerInfos.add(photographerInfo);
     }
 }

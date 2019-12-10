45237242c1b275409ebfbfc1a9cc82017967cb08
Tra <ungtra2814@gmail.com>
2019-12-10 18:03:22 +0700

    done basic 404 redirect

---
+++ src/main/java/com/fpt/t1708e/photoplatform/controller/DisErrorController.java
@@ -0,0 +1,39 @@
+package com.fpt.t1708e.photoplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DisErrorController{
+    @RequestMapping(method = RequestMethod.GET, value = "/error/admin/404")
+    public String error1(){
+        return "manager/admin/error/404";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error/admin/500")
+    public String error2() {
+        return "manager/admin/error/500";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error/admin/other")
+    public String error3(){
+        return "manager/admin/error/other";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error/404")
+    public String errorCustomer1(){
+        return "error/404";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error/500")
+    public String errorCustomer2(){
+        return "error/500";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error/other")
+    public String errorCustomer3(){
+        return "error/other";
+    }
+
+}
